66df30cca4e7c72a011dc12ee96d9accfee17547
RaushanImamAnsari <raushanimamansari@gmail.com>
2025-12-23 00:04:30 +0530

    Role-Based-Authorization(SpringSecurity)

--- src/main/java/com/mms/MMS/model/User.java
+++ src/main/java/com/mms/MMS/model/User.java
@@ -7,7 +7,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +26,4 @@ public class User {
     private List<UserEntry> userEntryList = new ArrayList<>();
 
     private List<String> roles = new ArrayList<>();
-
-
 }
--- src/main/java/com/mms/MMS/service/UserService.java
+++ src/main/java/com/mms/MMS/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
